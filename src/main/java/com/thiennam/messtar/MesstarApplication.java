1b982d4223ad3da7a324908f3b3262ed531461a5
ThienNam23 <trungnam23032001@gmail.com>
2022-01-21 15:40:43 +0700

    first commit

---
+++ src/main/java/com/thiennam/messtar/MesstarApplication.java
@@ -0,0 +1,13 @@
+package com.thiennam.messtar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesstarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiennam/messtar/MesstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiennam.messtar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
